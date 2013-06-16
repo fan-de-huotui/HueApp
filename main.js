154f9c0de31b1646ef5943ff3ac8ac111f58ff80
fan-de-huotui <fan.de.huotui@gmail.com>
2013-06-16 16:14:17 +0200

    only the phonegap www

--- main.js
+++ main.js
@@ -160,6 +160,6 @@ function toggleCompass() {
 function init() {
     // the next line makes it impossible to see Contacts on the HTC Evo since it
     // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
+    document.addEventListener("touchmove", preventBehavior, false);
     document.addEventListener("deviceready", deviceInfo, true);
 }
